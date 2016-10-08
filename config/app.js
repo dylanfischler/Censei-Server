36e34fcb215a0d5a74eaf0b5c5755c3e92009324
Lydia Cheah <cheahlizuan@gmail.com>
2016-10-08 17:06:15 -0400

    added watson stuff

--- config/app.js
+++ config/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const clarifaiLib = require('../lib/Clarifai/clarifai');
-// const watsonLib = require('../lib/Watson/watson');
+const watsonLib = require('../lib/Watson/watson');
 
 const configure = (app) => {
   if(!(process.env.CLARIFAI_CLIENT_ID && process.env.CLARIFAI_CLIENT_SECRET)) {
@@ -13,10 +13,10 @@ const configure = (app) => {
     clientSecret: process.env.CLARIFAI_CLIENT_SECRET
   });
 
-  // let watson = watsonLib({
-  //   username: process.env.SERVICE_NAME_USERNAME,
-  //   password: process.env.SERVICE_NAME_PASSWORD
-  // });
+  let watson = watsonLib({
+    username: process.env.SERVICE_NAME_USERNAME,
+    password: process.env.SERVICE_NAME_PASSWORD
+  });
 
   return app;
 }
