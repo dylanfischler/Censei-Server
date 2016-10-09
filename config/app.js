5dbc74244bc67f1e6041dd5f3a5ad2238121610e
Dylan Fischler <dylanfischler@gmail.com>
2016-10-09 00:41:42 -0400

    processing started

--- config/app.js
+++ config/app.js
@@ -4,41 +4,44 @@ const VERSION = 'V1';
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const multer = require('multer');
-// const upload = multer({ dest: 'uploads/' });
-
-// const busboy = require('connect-busboy');
-
 
 const clarifaiLib = require('../lib/Clarifai/clarifai');
 const watsonLib = require('../lib/Watson/watson');
 
+const verification = require('../lib/verification');
+
 const configure = (app) => {
   // check for required env
-  if(!(process.env.CLARIFAI_CLIENT_ID && process.env.CLARIFAI_CLIENT_SECRET)) {
+  if(!(process.env.CLARIFAI_CLIENT_ID 
+    && process.env.CLARIFAI_CLIENT_SECRET)) {
     console.error("Please provide Clarifai credentials (see README).");
     process.exit(0);
   }
+  else if(!(process.env.SERVICE_NAME_USERNAME 
+    && process.env.SERVICE_NAME_PASSWORD)) {
+    console.error("Please provide Watson credentials (see README).");
+    process.exit(0);
+  }
+
+  app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
+  app.use(bodyParser.json({limit: '50mb'}))
 
   let clarifai = clarifaiLib({
     clientId: process.env.CLARIFAI_CLIENT_ID,
     clientSecret: process.env.CLARIFAI_CLIENT_SECRET
   });
 
-  app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
-  app.use(bodyParser.json({limit: '50mb'}))
-
-  // let watson = watsonLib({
-  //   username: process.env.SERVICE_NAME_USERNAME,
-  //   password: process.env.SERVICE_NAME_PASSWORD
-  // });
+  let watson = watsonLib({
+    username: process.env.SERVICE_NAME_USERNAME,
+    password: process.env.SERVICE_NAME_PASSWORD
+  });
 
   app.post(`/api/${VERSION}/clarifai/predictImage`, (req, res) => {
     // console.log('has a body? ', (req.body.file));
     // console.log("processing image...", req.body.file);
     clarifai.predictImage({ image: req.body.file }).then(
       (response) => {
-        console.log("RESPONSE", JSON.stringify(response));
+        // console.log("RESPONSE", JSON.stringify(response));
         // res.status(200).send(response);
       }).catch((err) => {
         console.log("ERROR", err);
@@ -47,7 +50,45 @@ const configure = (app) => {
     });
   });
 
-  
+  app.post(`/api/${VERSION}/verify`, (req, res) => {
+    let errors = {}, results = {};
+
+    var checkResults = () => {
+      if(errors.clarifai || errors.watson) {
+        res.send(errors);
+      }
+      else if(results.clarifai && results.watson) {
+        // console.log("haz resultz", results);
+        verification.isValid(results);
+      }
+    }
+
+
+    clarifai.predictImage({ image: req.body.file }).then(
+      (response) => {
+        console.log("clarifai:got a response");
+        results.clarifai = response;
+        checkResults();
+      },
+      (error) => {
+        console.log("clarifai: error");
+        errors.clarifai = error;
+        checkResults();
+      });
+
+    watson.analyze({ text: req.body.text || "" }).then(
+      (response) => {
+        // results.watson = response;
+        console.log("watson:got a response");
+        results.watson = response;
+        checkResults();
+      },
+      (error) => {
+        console.log("watson:got a response");
+        errors.watson = error;
+        checkResults();
+      });
+  });
   
   app.get('/api/', (req, res) => {
     res.send("hello world");
--- test/testServer.js
+++ test/testServer.js
@@ -4,22 +4,26 @@ const express = require('express');
 const busboy = require('connect-busboy');
 const request = require('request');
 const concat = require('concat-stream');
+const censeiClient = require('censei-client');
 
 let app = express();
 
 app.use(busboy());
 
+const censei = censeiClient({ server_path: 'http://localhost:8080'});
+
 app.post('/uploadImage', (req, res) => {
-  var fstream;
   req.pipe(req.busboy);
   req.busboy.on('file', function (fieldName, fileStream, fileName, encoding, mimeType) {
     fileStream.pipe(concat(function (fileBuffer) {
-      request.post({
-        url: 'http://localhost:8080/api/V1/clarifai/predictImage'
-      }, function (err, r, body) {
-        // do handling here
-        res.send("Done!");
-      }).form({file: fileBuffer.toString('base64')})
+      censei.isThisSafe({ image: fileBuffer, text: "" });
+
+      // request.post({
+      //   url: 'http://localhost:8080/api/V1/clarifai/predictImage'
+      // }, function (err, r, body) {
+      //   // do handling here
+      //   res.send("Done!");
+      // }).form({file: fileBuffer.toString('base64')})
     }))
   });
 });
